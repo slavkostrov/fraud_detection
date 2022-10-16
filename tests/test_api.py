19380349795d4c198b70c88e788626c41abf013b
Slava <slavkostrov@gmail.com>
2022-10-17 00:11:13 +0300

    updated tests

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,10 +1,13 @@
-from fastapi.testclient import TestClient
-import os
-
+import random
 import sys
+
+import pytest
+from fastapi.testclient import TestClient
+
 sys.path.append("/home/runner/work/fraud_detection/fraud_detection/fastapi_practice/")
 
 from main import app
+
 
 def func(x):
     return x + 1
@@ -14,8 +17,41 @@ def test_answer():
     assert func(3) == 4
 
 
-
 client = TestClient(app)
+
+
 def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
+
+
+def test_predict():
+    for i in range(10):
+        transaction_id = random.randint(1, 100)
+        response = client.post(
+            f"/predict?transaction_id={transaction_id}",
+            headers={"Content-Type": "application/json", "accept": "application/json"},
+            json={
+                "TX_AMOUNT": random.random() * 1000,
+                "TX_TIME_SECONDS": random.random() * 1000,
+                "TERMINAL_ID": random.random() * 1000,
+                "CUSTOMER_ID": random.random() * 1000
+            },
+        )
+        assert response.status_code == 200
+        assert response.json()['is_fraud'] in [0, 1]
+
+
+def test_errors():
+    response = client.post(
+        f"/predict?transaction_id=1",
+        headers={"Content-Type": "application/json", "accept": "application/json"},
+        json={
+            "TX_AMOUNT": "x",
+            "TX_TIME_SECONDS": random.random() * 1000,
+            "TERMINAL_ID": "y",
+            "CUSTOMER_ID": random.random() * 1000
+        },
+    )
+
+    assert response.status_code != 200
