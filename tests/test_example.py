a77e3242cf31a6f1bad178958432ff0dc4ecbbbf
slavkostrov <64536258+slavkostrov@users.noreply.github.com>
2022-10-16 20:26:24 +0300

    Update test_example.py

--- tests/test_example.py
+++ tests/test_example.py
@@ -1,14 +1,17 @@
 def func(x):
     return x + 1
 
+import os
+MODEL_NAME = os.getenv("MODEL_NAME", default="fraud_detection_sklearn_model")
+MODEL_VERSION = os.getenv("MODEL_VERSION", default="latest")
 
 def test_answer():
     assert func(3) == 4
 
 def test_model():
+    # check if model can be loaded 
     from sklearn.pipeline import Pipeline
     import mlflow
-    import os
     
     mlflow.set_tracking_uri(os.getenv("MLFLOW_URI"))
     loaded_model = mlflow.pyfunc.load_model(f"models:/{MODEL_NAME}/{MODEL_VERSION}")
