e4935e541312ac127a760cf106d065174fe54645
slavkostrov <64536258+slavkostrov@users.noreply.github.com>
2022-10-16 19:02:34 +0300

    Update main.py

--- fastapi_practice/main.py
+++ fastapi_practice/main.py
@@ -15,11 +15,11 @@ app = FastAPI()
 MODEL_NAME = os.getenv("MODEL_NAME", default="fraud_detection_sklearn_model")
 MODEL_VERSION = os.getenv("MODEL_VERSION", default="latest")
 
-mlflow.set_tracking_uri(f"")
-os.environ["AWS_ACCESS_KEY_ID"] = ""
-os.environ["AWS_SECRET_ACCESS_KEY"] = ""
-os.environ["MLFLOW_S3_ENDPOINT_URL"] = ""
-os.environ["AWS_S3_ENDPOINT_URL"] = ""
+mlflow.set_tracking_uri(os.getenv("MLFLOW_URI"))
+# os.environ["AWS_ACCESS_KEY_ID"] = ""
+# os.environ["AWS_SECRET_ACCESS_KEY"] = ""
+# os.environ["MLFLOW_S3_ENDPOINT_URL"] = ""
+# os.environ["AWS_S3_ENDPOINT_URL"] = ""
 
 
 class Model:
