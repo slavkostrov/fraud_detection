732c93e25ad9d767c6b983e875b3cb96dfd4d926
Slava <slavkostrov@gmail.com>
2022-10-16 23:40:52 +0300

    updated main

--- fastapi_practice/main.py
+++ fastapi_practice/main.py
@@ -3,32 +3,65 @@
 import os
 from typing import Optional, List
 
-import numpy as np
 import pandas as pd
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from sklearn.pipeline import Pipeline
-import mlflow
+
+
+def current_environment():
+    """get current environment type from env variable."""
+    return os.getenv("APP_ENV", default="TEST")
+
+
+def _load_model():
+    """load model based on current environment specific"""
+    env = current_environment()
+    if env == "TEST":
+        import boto3
+        session = boto3.session.Session()
+        s3 = session.client(
+            service_name='s3',
+            endpoint_url='https://storage.yandexcloud.net'
+        )
+        s3.download_file('slava-otus', 'models/testmodel.pkl', 'model.pkl')
+        with open("model.pkl", "rb") as file:
+            import pickle
+            model = pickle.load(file)
+        return model, None
+    elif env == "PROD":
+        import mlflow
+        mlflow.set_tracking_uri(os.getenv("MLFLOW_URI"))
+        loaded_model = mlflow.pyfunc.load_model(f"models:/{MODEL_NAME}/{MODEL_VERSION}")
+        feature_list = loaded_model._model_meta._signature.inputs.input_names()
+        pipeline = loaded_model._model_impl
+        return pipeline, feature_list
+    else:
+        raise RuntimeError(f"Setup `APP_ENV` variable (Test, Prod).")
+
 
 app = FastAPI()
 
 MODEL_NAME = os.getenv("MODEL_NAME", default="fraud_detection_sklearn_model")
 MODEL_VERSION = os.getenv("MODEL_VERSION", default="latest")
 
-mlflow.set_tracking_uri(os.getenv("MLFLOW_URI"))
 if (
-    os.getenv("AWS_ACCESS_KEY_ID") is None or 
-    os.getenv("AWS_SECRET_ACCESS_KEY") is None or
-    os.getenv("MLFLOW_S3_ENDPOINT_URL") is None or
-    os.getenv("MLFLOW_URI") is None or
-    os.getenv("AWS_S3_ENDPOINT_URL") is None
+        os.getenv("AWS_ACCESS_KEY_ID") is None or
+        os.getenv("AWS_SECRET_ACCESS_KEY") is None or
+        os.getenv("MLFLOW_S3_ENDPOINT_URL") is None or
+        os.getenv("AWS_S3_ENDPOINT_URL") is None
 ):
-    raise RuntimeError("Missing env variable (AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, MLFLOW_S3_ENDPOINT_URL, AWS_S3_ENDPOINT_URL, MLFLOW_URI).")
+    raise RuntimeError(
+        "Missing env variable (AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, MLFLOW_S3_ENDPOINT_URL, AWS_S3_ENDPOINT_URL, MLFLOW_URI).")
 
+if os.getenv("MLFLOW_URI") is None and current_environment():
+    raise RuntimeError("Setup `MLFLOW_URI` for production env to get_models.")
 
 class Model:
     pipeline: Optional[Pipeline] = None
-    feature_list: List[str]
+    feature_list: List[str] = ["f_TX_AMOUNT", "f_TX_TIME_SECONDS", "f_mean_customer_TX_AMOUNT",
+                               "f_mean_terminal_TX_AMOUNT", "f_unq_terminals_per_customer", "f_customer_amount_ratio",
+                               "f_terminal_amount_ratio"]
 
 
 class CustomerFeatures:
@@ -47,14 +80,24 @@ class Transaction(BaseModel):
 
 
 @app.on_event("startup")
+def load_features():
+    import boto3
+    s3 = boto3.client('s3', endpoint_url='https://storage.yandexcloud.net')
+    for features_file in "customer_features.parquet", "terminal_features.parquet":
+        s3.download_file('slava-otus', f'features/{features_file}', features_file)
+
+
+@app.on_event("startup")
 def load_model():
-    loaded_model = mlflow.pyfunc.load_model(f"models:/{MODEL_NAME}/{MODEL_VERSION}")
-    Model.feature_list = loaded_model._model_meta._signature.inputs.input_names()
-    Model.pipeline = loaded_model._model_impl
+    model, feature_list = _load_model()
+
+    Model.pipeline = model
+    if feature_list is not None:
+        """for test model feature list is static"""
+        Model.feature_list = feature_list
 
     CustomerFeatures.features = pd.read_parquet("customer_features.parquet")
     TerminalFeatures.features = pd.read_parquet("terminal_features.parquet")
-
 
 
 @app.get("/")
@@ -65,7 +108,7 @@ def read_healthcheck():
 @app.post("/predict")
 def predict(transaction_id: int, transaction: Transaction):
     df = pd.DataFrame([transaction.dict()])
-    
+
     if Model.pipeline is None:
         raise HTTPException(status_code=503, detail="No model loaded")
 
@@ -77,10 +120,10 @@ def predict(transaction_id: int, transaction: Transaction):
 
     df = df.merge(CustomerFeatures.features, on="CUSTOMER_ID")
     print("added customer features")
-    
+
     df = df.merge(TerminalFeatures.features, on="TERMINAL_ID")
     print("added terminal features")
-    
+
     df["f_customer_amount_ratio"] = df["TX_AMOUNT"] / df["f_mean_customer_TX_AMOUNT"]
     df["f_terminal_amount_ratio"] = df["TX_AMOUNT"] / df["f_mean_terminal_TX_AMOUNT"]
 
