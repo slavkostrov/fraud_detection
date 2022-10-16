f60e28c8a91db53c94ea670a898c668590310cc1
Slava <slavkostrov@gmail.com>
2022-10-17 00:19:41 +0300

    updated tests

--- tests/test_api.py
+++ tests/test_api.py
@@ -29,15 +29,17 @@ def test_predict():
     for i in range(10):
         transaction_id = random.randint(1, 100)
         response = client.post(
-            f"/predict?transaction_id={transaction_id}/",
+            f"/predict/",
             headers={"Content-Type": "application/json", "accept": "application/json"},
             json={
+                "transaction_id": transaction_id,
                 "TX_AMOUNT": random.random() * 1000,
                 "TX_TIME_SECONDS": random.random() * 1000,
                 "TERMINAL_ID": random.random() * 1000,
                 "CUSTOMER_ID": random.random() * 1000
             },
         )
+        print(response.json())
         assert response.status_code == 200
         assert response.json()['is_fraud'] in [0, 1]
 
