f3c70ee70c5c3a0209aec7bc6fee7af6a4c9da48
slavkostrov <64536258+slavkostrov@users.noreply.github.com>
2022-10-16 23:47:07 +0300

    Update and rename test_example.py to test_api.py

---
+++ tests/test_api.py
@@ -0,0 +1,21 @@
+from fastapi.testclient import TestClient
+import os
+
+import sys
+sys.path.append("../fastapi_practice/")
+
+from main import app
+
+def func(x):
+    return x + 1
+
+
+def test_answer():
+    assert func(3) == 4
+
+
+
+client = TestClient(app)
+def test_read_main():
+    response = client.get("/")
+    assert response.status_code == 200
